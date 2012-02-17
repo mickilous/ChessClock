612ccb72a3345d13e5c6548920f10c69cfceb512
Claude Michiels - Mick <mickilous@gmail.com>
2012-02-17 16:40:27 +0100

    Game Over Vibration

--- src/com/crazyapps/chessclock/manager/Notifier.java
+++ src/com/crazyapps/chessclock/manager/Notifier.java
@@ -8,15 +8,17 @@ import com.crazyapps.chessclock.R;
 
 public class Notifier {
 
-	private boolean		isSoundOnClick;
-	private boolean		isVibrateOnClick;
-	private boolean		isSoundOnGameOver;
-	private boolean		isVibrateOnGameOver;
-	private Vibrator	vibrator;
-	private MediaPlayer	clickMediaPlayer;
-	private MediaPlayer	gameOverMediaPlayer;
+	private final long[]	gameOverPattern	= { 0L, 1000L, 100L, 1000L, 100L, 1000L };
 
-	private Context		context;
+	private boolean			isSoundOnClick;
+	private boolean			isVibrateOnClick;
+	private boolean			isSoundOnGameOver;
+	private boolean			isVibrateOnGameOver;
+	private Vibrator		vibrator;
+	private MediaPlayer		clickMediaPlayer;
+	private MediaPlayer		gameOverMediaPlayer;
+
+	private Context			context;
 
 	public Notifier(Context context) {
 		this.context = context;
@@ -26,6 +28,8 @@ public class Notifier {
 		vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
 		clickMediaPlayer = MediaPlayer.create(context, R.raw.click);
 		gameOverMediaPlayer = MediaPlayer.create(context, R.raw.game_over);
+		// clickMediaPlayer.prepareAsync();
+		// gameOverMediaPlayer.prepareAsync();
 	}
 
 	public void release() {
@@ -34,17 +38,18 @@ public class Notifier {
 	}
 
 	public void click() {
-		if (isVibrateOnClick)
-			vibrator.vibrate(100);
 		if (isSoundOnClick)
 			clickMediaPlayer.start();
+		if (isVibrateOnClick)
+			vibrator.vibrate(100);
 	}
 
 	public void gameOver() {
-		if (isVibrateOnGameOver)
-			vibrator.vibrate(300);
 		if (isSoundOnGameOver)
 			gameOverMediaPlayer.start();
+		if (isVibrateOnGameOver) {
+			vibrator.vibrate(gameOverPattern, -1);
+		}
 	}
 
 	public void setSoundOnClick(boolean isSoundOnClick) {
